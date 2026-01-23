6316e6c2de15ce3a25680e9eb3a948cd93a17732
Fahadsule <fahadsuleiman31@gmail.com>
2026-01-23 21:10:59 +0300

    JUST DID THE DAILY UPDATE. 2026-01-23

--- main_update_pipeline.py
+++ main_update_pipeline.py
@@ -27,11 +27,11 @@ def run_sql_file(db_path, sql_file):
 if __name__=="__main__":
     DB_PATH="db/market_data.db"
     run_script("DSE/scripts/dse_equities_updates.py")
-    run_sql_file(DB_PATH,"DSE/scripts/autofill_dse.sql")
+    #run_sql_file(DB_PATH,"DSE/scripts/autofill_dse.sql")
     run_script("NSE/nse_equities_updates.py")
-    run_sql_file(DB_PATH,"NSE/autofill_nse.sql")
+    #run_sql_file(DB_PATH,"NSE/autofill_nse.sql")
     run_script("JSE/jse_scripts/jse_equities_updates.py")
     run_script("JSE/jse_scripts/jse_indices_updates.py")
-    run_sql_file(DB_PATH,"JSE/jse_scripts/autofill_jse.sql")
+    #run_sql_file(DB_PATH,"JSE/jse_scripts/autofill_jse.sql")
     run_script("BRVM/scripts/brvm_page.py")
     run_script("BRVM/scripts/brvm_equities_updates.py")
